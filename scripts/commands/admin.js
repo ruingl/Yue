5ebc7bd350fd1e28102a4f2d40657eb90553654d
Yue。るい <149220283+ruingl@users.noreply.github.com>
2023-11-01 14:24:43 +0800

    Admin.js Stable

--- scripts/commands/admin.js
+++ scripts/commands/admin.js
@@ -1,33 +1,38 @@
 const fs = require("fs");
-const path = require("path");
-
-const ADMIN_FILE_PATH = "../../admin.json";
 
 module.exports = {
   config: {
     name: "admin",
-    description: "Displays a list of admins.",
-    usage: ":admin [add/remove] [userID]",
+    description: "Displays a list of admins or manages admins.",
+    usage: ":admin [add/remove/list] [userID]",
     author: "Rui",
-    role: 1, // Set the role to 0 for everyone or 1 for admins only
+    role: 0, // Set the role to 0 for everyone to use this command
   },
 
-  run: ({ api, event, args }) => {
+  run: async ({ api, event, args, box }) => {
     const [action, userID] = args;
+    const adminList = JSON.parse(fs.readFileSync('admin.json', 'utf8')).admins;
+
+    if (!adminList.some(admin => admin.id === event.senderID)) {
+      box.reply('You do not have permission to use this command.');
+      return;
+    }
 
     if (action === "add") {
-      addAdmin(api, event, userID);
+      addAdmin(api, event, userID, box);
     } else if (action === "remove") {
-      removeAdmin(api, event, userID);
+      removeAdmin(api, event, userID, box);
+    } else if (action === "list") {
+      listAdmins(api, event, box);
     } else {
-      api.sendMessage("Invalid usage. Please use :admin [add/remove] [userID]", event.threadID, event.messageID);
+      box.reply("Invalid usage. Please use :admin [add/remove/list] [userID]");
     }
   }
 };
 
 function loadAdmins() {
   try {
-    const adminData = fs.readFileSync(path.resolve(__dirname, ADMIN_FILE_PATH));
+    const adminData = fs.readFileSync('admin.json', 'utf8');
     const admins = JSON.parse(adminData).admins;
     return admins;
   } catch (error) {
@@ -38,30 +43,40 @@ function loadAdmins() {
 
 function saveAdmins(admins) {
   const data = { admins: admins };
-  fs.writeFileSync(path.resolve(__dirname, ADMIN_FILE_PATH), JSON.stringify(data, null, 2));
+  fs.writeFileSync('admin.json', JSON.stringify(data, null, 2));
 }
 
-function addAdmin(api, event, userID) {
+function addAdmin(api, event, userID, box) {
   let admins = loadAdmins();
 
   if (!admins.some(admin => admin.id === userID)) {
     admins.push({ id: userID, name: `Admin${admins.length + 1}` });
     saveAdmins(admins);
-    api.sendMessage(`Admin with ID ${userID} has been added.`, event.threadID);
+    box.reply(`Admin with ID ${userID} has been added.`);
   } else {
-    api.sendMessage(`User with ID ${userID} is already an admin.`, event.threadID);
+    box.reply(`User with ID ${userID} is already an admin.`);
   }
 }
 
-function removeAdmin(api, event, userID) {
+function removeAdmin(api, event, userID, box) {
   let admins = loadAdmins();
 
   const index = admins.findIndex(admin => admin.id === userID);
   if (index !== -1) {
     const removedAdmin = admins.splice(index, 1)[0];
     saveAdmins(admins);
-    api.sendMessage(`Admin with ID ${removedAdmin.id} has been removed.`, event.threadID);
+    box.reply(`Admin with ID ${removedAdmin.id} has been removed.`);
   } else {
-    api.sendMessage(`User with ID ${userID} is not an admin.`, event.threadID);
+    box.reply(`User with ID ${userID} is not an admin.`);
+  }
+}
+
+function listAdmins(api, event, box) {
+  let admins = loadAdmins();
+  if (admins.length > 0) {
+    const adminList = admins.map(admin => `${admin.name} (ID: ${admin.id})`).join("\n");
+    box.reply(`List of admins:\n${adminList}`);
+  } else {
+    box.reply("No admins found.");
   }
 }
