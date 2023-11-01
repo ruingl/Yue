90bfa3fd715e64c1864c2bddc073b158230605ee
Yue。るい <149220283+ruingl@users.noreply.github.com>
2023-11-01 13:55:29 +0800

    Update eval.js

--- scripts/commands/eval.js
+++ scripts/commands/eval.js
@@ -1,22 +1,30 @@
+const fs = require('fs');
+
 module.exports = {
   config: {
-    name: "eval",
-    description: "Executes the provided JavaScript code",
-    usage: ":eval <code>",
-    author: "LiANE",
+    name: 'eval',
+    description: 'Executes the provided JavaScript code',
+    usage: ':eval <code>',
+    author: 'LiANE',
+    role: 1,
   },
-  run: ({ api, event }) => {
+  run: async ({ api, event, box }) => {
     try {
-const args = event.body.split(" ");
-      const code = args.slice(1).join(" ");
-      eval(code);
+      const adminList = JSON.parse(fs.readFileSync('admin.json', 'utf8')).admins;
 
+      if (!adminList.some(admin => admin.id === event.senderID)) {
+        box.reply('You do not have permission to use this command.');
+        return;
+      }
+
+      const args = event.body.split(' ');
+      const code = args.slice(1).join(' ');
+      eval(code);
     } catch (error) {
-      api.sendMessage(`🔥 | Oops! Nag error:
+      box.reply(`🔥 | Oops! may iror
 Error: ${error.message}
 
-Body:
-${code}`, event.threadID);
+`);
     }
   },
 };
